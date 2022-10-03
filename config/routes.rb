49be96e48872acdac84c6d8b61391938f554a341
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-03 19:02:44 +0100

    generate devise gem for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'users#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
