89609134bb01869c9638e5ee6d3574c1e0cc6bbc
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-03 19:17:17 +0100

    create database, migration and associate model relation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_123041) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_172334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_123041) do
     t.string "categname"
     t.string "image"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
