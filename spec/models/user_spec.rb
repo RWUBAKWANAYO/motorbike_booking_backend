3c7a02219acbffd9829dbf2f7f8662496130c11c
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-03 20:24:18 +0100

    Create validation specs using Rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(email: 'test@test.com', password: 'hello2WORLD', name: 'test', role: 'admin') }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is valid without a role (default role = user)' do
+    subject.role = nil
+    expect(subject).to be_valid
+  end
 end
