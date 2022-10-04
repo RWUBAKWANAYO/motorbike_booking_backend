0642b2dc39715f01cbe14d98a495f51545db23d4
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-04 12:49:16 +0100

    generate user controller

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.2'
 
 gem 'devise'
-
+gem 'jwt'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.3'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'users#index'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
