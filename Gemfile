b173cbd5fdf486a6c04f065a6636224edec05447
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-04 17:31:47 +0100

    Add jwt and bcrypt gems

--- Gemfile
+++ Gemfile
@@ -4,7 +4,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.2'
 
 gem 'devise'
+
 gem 'jwt'
+
+gem 'bcrypt'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.3'
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  namespace :api do
+    resources :users, only:  [:index, :show] do
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
