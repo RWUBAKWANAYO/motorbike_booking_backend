9a8fb2c76d048a608e121b3852e1ddc0913f1956
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-04 18:13:14 +0100

    Modify test file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users#sessions'
   }
 
   namespace :api do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET #index' do
+    before(:each) { get users_path }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+    it 'includes correct placeholder text' do
+      expect(response).to have_http_status(:ok)
+    end
   end
-end
