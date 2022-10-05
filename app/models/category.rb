f33ce4fa9b723a2237f98deffcd200f44dcf2bad
Pedro Marques <ppmarq1@gmail.com>
2022-10-05 11:07:58 +0100

    Add motorbike model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
-  # has_many :motorbikes, dependent: :destroy
+  has_many :motorbikes, dependent: :destroy
   validates :categname, presence: true, uniqueness: true
-  # has_one_attached :image
 end
