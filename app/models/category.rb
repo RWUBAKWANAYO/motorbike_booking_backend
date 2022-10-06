3b5a213b0aafeb1be9497473e973cc8c5114f9c7
Pedro Marques <ppmarq1@gmail.com>
2022-10-06 10:35:57 +0100

    Add specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  # has_many :motorbikes, dependent: :destroy
+  has_many :motorbikes, dependent: :destroy
   validates :categname, presence: true, uniqueness: true
   # has_one_attached :image
 end
--- app/models/motorbike.rb
+++ app/models/motorbike.rb
@@ -1,7 +1,7 @@
 class Motorbike < ApplicationRecord
   belongs_to :category
-  # has_many :reservations, dependent: :destroy
-  # has_many :users, through: :reservations
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations
 
   validates :brand, :year, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
