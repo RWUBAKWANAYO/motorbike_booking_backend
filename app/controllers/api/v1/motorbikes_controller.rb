b74019cba0a1e883592682ab7b56ee9d48b153aa
Pedro Marques <ppmarq1@gmail.com>
2022-10-07 12:11:18 +0100

    Update motorbike controller

--- app/controllers/api/v1/motorbikes_controller.rb
+++ app/controllers/api/v1/motorbikes_controller.rb
@@ -2,45 +2,48 @@ class Api::V1::MotorbikesController < ApplicationController
   # before_action :logged_in
 
   def index
-    motorbikes = Motorbike.all.order(created_at: :desc)
-    if motorbikes
-      render json: motorbikes, include: [:reservations]
+    @motorbikes = Motorbike.all.order(created_at: :desc)
+    if @motorbikes
+      render json: @motorbikes, include: [:reservations]
     else
-      render json: { error: 'There is no motorbikes' }
+      render json: { message: 'There is no motorbikes', errors: @motorbikes.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
   def show
-    motorbike = Motorbike.find_by_id(params[:id])
-    if motorbike
-      render json: motorbike, include: [:reservations]
+    @motorbike = @motorbike.find_by_id(params[:id])
+    if @motorbike
+      render json: @motorbike, include: [:reservations]
     else
-      render json: { error: 'Unable to find motorbike' }
+      render json: { message: 'Unable to find @motorbike', errors: @motorbike.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
   def create
-    motorbike = Motorbike.new(motorbike_params)
-    if motorbike.save
+    @motorbike = Motorbike.new(motorbike_params)
+    if @motorbike.save
       render json: { message: 'Motorbike created successfully' }
     else
-      render json: { message: 'Error creating motorbike', errors: motorbike.errors.full_messages },
+      render json: { message: 'Error creating motorbike', errors: @motorbike.errors.full_messages },
              status: :unprocessable_entity
     end
   end
 
   def destroy
-    motorbike = Motorbike.find_by_id(params[:id])
-    if motorbike.destroy
+    @motorbike = @motorbike.find_by_id(params[:id])
+    if @motorbike.destroy
       render json: { message: 'Motorbike deleted successfully' }
     else
-      render json: { error: 'Error deleting motorbike' }
+      render json: { message: 'Error deleting motorbike', errors: @motorbike.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
   def update
-    motorbike = Motorbike.find_by_id(params[:id])
-    if motorbike.update(reserved_params)
+    @motorbike = Motorbike.find_by_id(params[:id])
+    if @motorbike.update(reserved_params)
       render json: { message: 'Motorbike updated successfully' }
     else
       render json: { error: 'Error updating motorbike' }
