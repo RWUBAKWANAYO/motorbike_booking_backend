93842e61f9866270faa659a763835991714c54e3
Pedro Marques <ppmarq1@gmail.com>
2022-10-11 11:07:29 +0100

    Revert "Create Reservation controller"

--- db/migrate/20221010133520_change_col_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColName < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :motorbikes, :brand, :motor_name 
-  end
-end
--- spec/requests/reservation_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Reservations', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
