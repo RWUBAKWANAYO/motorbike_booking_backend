d22944ee718acf9ddb7833f6d80930932c732429
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-10-11 15:44:51 +0100

    Install rswag generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :adverts
   devise_for :users, controllers: {
     sessions: 'users/sessions',
