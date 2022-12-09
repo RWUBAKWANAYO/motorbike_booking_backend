85b40665f5785c828e240b545e61ca891f338217
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-12-09 16:55:12 +0200

    change credentials

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_10_10_133520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "adverts", force: :cascade do |t|
     t.string "name"
